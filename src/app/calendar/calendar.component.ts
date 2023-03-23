be4448f3d2aa16f4b2dae36193b954feecc36fe7
hedilbnr <hedil.bromdhane@gmail.com>
2023-03-23 09:28:10 +0000

    frontend

---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent implements OnInit {
+  calendarDays: Date[] = [];
+
+  ngOnInit() {
+    this.generateCalendar();
+  }
+
+  generateCalendar() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= daysInMonth; i++) {
+      const date = new Date(year, month, i);
+      this.calendarDays.push(date);
+    }
+  }
+}
